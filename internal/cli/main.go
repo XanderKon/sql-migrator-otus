80b4b54c59978cb220e7cff71438ba849c58d540
Кондратенко Александр <kondratenkoaa2@edu.mos.ru>
2024-01-18 19:21:51 +0300

    feat: Базовое описание cli-команды

--- internal/cli/main.go
+++ internal/cli/main.go
@@ -18,16 +18,42 @@ func init() {
 
 func initFlags() {
 	configFile = flag.Lookup("config").Value.(flag.Getter).Get().(string)
+
+	flag.Usage = func() {
+		fmt.Fprintf(
+			os.Stderr,
+			`Usage: gomigrator [OPTIONS] COMMAND [arg...]
+  
+  You can override varuables from config file by ENV, just use something like "${DB_DSN}"
+
+  OPTIONS:
+    -config         Path to configuration file
+		
+  COMMAND:
+    create [name]   Create migration with 'name'
+    up              Migrate the DB to the most recent version available
+    down            Roll back the version by 1
+    redo            Re-run the latest migration
+    status          Print all migrations status
+    dbversion       Print migrations status (last applied migration)
+    help            Print usage
+    version         Application version
+
+  Examples:
+    gomigrator -config="../configs/config-test.yml" create "create_user_table"
+    DB_DSN="postgresql://app:test@pgsql:5432/app" gomigrator up
+
+Feel free to put PR here: https://github.com/XanderKon/sql-migrator-otus
+
+Inspired by:
+https://github.com/pressly/goose
+https://github.com/golang-migrate/migrate
+`)
+	}
 }
 
 func Main() {
-	flag.Parse()
 	initFlags()
-
-	if flag.Arg(0) == "version" {
-		printVersion()
-		return
-	}
 
 	config := NewConfig()
 
@@ -35,6 +61,5 @@ func Main() {
 
 	logg.Info("Start migrator app")
 
-	fmt.Println("Configuration: ", config.Migrator.DSN, config.Migrator.Dir, config.Migrator.Type, config.Logger.Level)
-
+	flag.Usage()
 }
