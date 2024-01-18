e451c52e5917f573e65ad0970d7a469e54c0d6e8
Кондратенко Александр <kondratenkoaa2@edu.mos.ru>
2024-01-18 18:57:23 +0300

    refactor: printVersion вынесен обратно в main пакет

--- cmd/gomigrator/main.go
+++ cmd/gomigrator/main.go
@@ -1,9 +1,18 @@
 package main
 
 import (
+	"flag"
+
 	"github.com/XanderKon/sql-migrator-otus/internal/cli"
 )
 
 func main() {
+	flag.Parse()
+
+	if flag.Arg(0) == "version" {
+		printVersion()
+		return
+	}
+
 	cli.Main()
 }
